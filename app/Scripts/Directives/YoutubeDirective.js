50206b67281885e8b31bd5c12bfdddad03094cb7
Dixit <dixit.2608@gmail.com>
2016-02-18 12:12:14 +0530

    Change in YoutubeDirective  api call related code move from link to controller

--- app/Scripts/Directives/YoutubeDirective.js
+++ app/Scripts/Directives/YoutubeDirective.js
@@ -7,29 +7,15 @@ angular.module('TrippismUIApp').directive('youtubeInfo', ['$compile', '$timeout'
                 youtubeParams: '=',
             },
             templateUrl: '/Views/Partials/YoutubePartial.html',
-            controller: function ($scope) {
-
-            },
-            link: function (scope, elem, attrs) {
-                var youtubeTblheight = ($(window).height() - 100) + 'px';
-                $('.ytubetabletbody').each(function (i, obj) {
-                    $(this).css('height', youtubeTblheight);
-                });
-
-                scope.youtubeInfoDataFound = false;
-                scope.$watchGroup(['youtubeParams'], function (newValue, oldValue, scope) {
-                    scope.youtubeData = "";
-                    scope.loadyoutubeInfo();
-                });
-
+            controller: ['$scope', function (scope) {
                 scope.curPage = 0;
                 scope.pageSize = 10;
                 scope.isDataLoadedFirstTime = false;
 
-                scope.loadyoutubeInfo = function (nextTokenID,prevTokenID) {
+                scope.loadyoutubeInfo = function (nextTokenID, prevTokenID) {
                     scope.youtubeInfoLoaded = false;
                     scope.youtubeInfoDataFound = false;
-                   // scope.youtubeData = "";
+                    // scope.youtubeData = "";
                     if (scope.youtubeParams != undefined) {
                         var parameters = scope.youtubeParams.DestinationairportName.airport_Lat + "," + scope.youtubeParams.DestinationairportName.airport_Lng;
                         if (nextTokenID != null) {
@@ -46,34 +32,49 @@ angular.module('TrippismUIApp').directive('youtubeInfo', ['$compile', '$timeout'
                         };
 
                         if (scope.youtubeInfoLoaded == false) {
-                                scope.youtubepromise = YouTubeFactory.youTube(data).then(function (data) {
-                                    if (data.status == 404) {
-                                        scope.youtubeInfoDataFound = false;
-                                        return;
-                                    }
-                                    scope.isDataLoadedFirstTime = true;
-                                    scope.youtubeData = data;
-                                   
-                                    if (scope.youtubeData.nextPageToken != null)
-                                        scope.nextToken = scope.youtubeData.nextPageToken;
-                                    
-                                    if (scope.youtubeData.prevPageToken != null)
-                                        scope.prevToken = scope.youtubeData.prevPageToken;
-                                    
-                                    scope.numberOfPages = Math.ceil(scope.youtubeData.pageInfo.totalResults / scope.pageSize);
-                                    if((scope.curPage + 1) >= scope.numberOfPages)
-                                        scope.isdisabled = 0;
-                                    else
-                                        scope.isdisabled = 1;
+                            scope.youtubepromise = YouTubeFactory.youTube(data).then(function (data) {
+                                if (data.status == 404) {
+                                    scope.youtubeInfoDataFound = false;
+                                    return;
+                                }
+                                scope.isDataLoadedFirstTime = true;
+                                scope.youtubeData = data;
 
-                                    scope.youtubeInfoDataFound = true;
+                                if (scope.youtubeData.nextPageToken != null)
+                                    scope.nextToken = scope.youtubeData.nextPageToken;
 
-                                    loadScrollbars();
-                                });
+                                if (scope.youtubeData.prevPageToken != null)
+                                    scope.prevToken = scope.youtubeData.prevPageToken;
+
+                                scope.numberOfPages = Math.ceil(scope.youtubeData.pageInfo.totalResults / scope.pageSize);
+                                if ((scope.curPage + 1) >= scope.numberOfPages)
+                                    scope.isdisabled = 0;
+                                else
+                                    scope.isdisabled = 1;
+
+                                scope.youtubeInfoDataFound = true;
+
+                                loadScrollbars();
+                            });
                         }
                         scope.youtubeInfoLoaded = true;
                     }
                 };
+            }],
+            link: function (scope, elem, attrs) {
+                var youtubeTblheight = ($(window).height() - 100) + 'px';
+                $('.ytubetabletbody').each(function (i, obj) {
+                    $(this).css('height', youtubeTblheight);
+                });
+
+                scope.youtubeInfoDataFound = false;
+                scope.$watchGroup(['youtubeParams'], function (newValue, oldValue, scope) {
+                    scope.youtubeData = "";
+                    scope.loadyoutubeInfo();
+                });
+
+              
+            
             }
         }
     }]);

066073db73220eeca1fbafdcefcd0c2a40f7840a
tgsravi <ravi.p@trivenitechnologies.in>
2016-03-07 18:56:09 +0530

    Working on bargain finder

--- app/Scripts/controllers/InstaFlightSearchController.js
+++ app/Scripts/controllers/InstaFlightSearchController.js
@@ -1,8 +1,8 @@
 ﻿(function () {
     'use strict';
     var controllerId = 'InstaFlightSearchController';
-    angular.module('TrippismUIApp').controller(controllerId, ['$scope', '$filter', '$modal', 'InstaFlightSearchFactory', 'UtilFactory', 'instaFlightSearchData', InstaFlightSearchController]);
-    function InstaFlightSearchController($scope, $filter, $modal, InstaFlightSearchFactory, UtilFactory, instaFlightSearchData) {
+    angular.module('TrippismUIApp').controller(controllerId, ['$scope', '$filter', '$modal', 'BargainFinderMaxFactory', 'UtilFactory', 'instaFlightSearchData', InstaFlightSearchController]);
+    function InstaFlightSearchController($scope, $filter, $modal, BargainFinderMaxFactory, UtilFactory, instaFlightSearchData) {
         $scope.isInstaFlightDataFound = null;
         $scope.instaFlightSearchData = instaFlightSearchData;
         var airportNameCache = [];

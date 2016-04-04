82d49a7d682857936a283af849853d00cac54051
tgsravi <ravi.p@trivenitechnologies.in>
2016-04-04 17:41:08 +0530

    working on jssor slider

--- app/Scripts/controllers/HomeController.js
+++ app/Scripts/controllers/HomeController.js
@@ -4,12 +4,14 @@
     angular.module('TrippismUIApp').controller(controllerId,
          ['$scope',
             '$window',
+            '$timeout',
             'LocalStorageFactory',
             'TrippismConstants',
              HomeController]);
     function HomeController(
        $scope,
        $window,
+       $timeout,
        LocalStorageFactory,
        TrippismConstants
        ) {
@@ -19,15 +21,21 @@
 
         // set page height based on window's height
         var w = angular.element($window);
+        var pageHeight;
+        var pageWidth;
         function setPageHeight() {
             var boxwrap = angular.element("#destination-boxwrap");
             var h50px = angular.element("#h50px");
-            boxwrap.height(w.height() - h50px.height());
+            pageHeight = w.height() - h50px.height();
+            pageWidth = boxwrap.width();
+            boxwrap.height(pageHeight);
         }
 
         setPageHeight();
         w.bind('resize', function () {
             setPageHeight();
+            setSliderSize();
+            jssor_1_slider_init();
         });
 
         angular.element('#carousel').carousel({
@@ -37,5 +45,30 @@
 
         LocalStorageFactory.clear(TrippismConstants.refineSearchLocalStorage);
         // --- Ends----
+
+
+        function setSliderSize() {
+            angular.element('#jssor_slider, #jssor_1').css({ width: pageWidth + 'px', height: pageHeight + 'px' });
+        }
+
+        var jssor_1_slider_init = function () {
+            var jssor_1_SlideshowTransitions = [
+              { $Duration: 4000, $Opacity: 2 }
+            ];
+
+            var jssor_1_options = {
+                $AutoPlay: true,
+                $SlideshowOptions: {
+                    $Class: $JssorSlideshowRunner$,
+                    $Transitions: jssor_1_SlideshowTransitions,
+                    $TransitionsOrder: 1
+                }
+            };
+
+            var jssor_1_slider = new $JssorSlider$("jssor_slider", jssor_1_options);
+        };
+
+        setSliderSize();
+        jssor_1_slider_init();
     }
 })();

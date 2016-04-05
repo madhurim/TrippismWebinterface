4373bfdd4ee4786f912070d928d3d9cfa2f41eca
tgsravi <ravi.p@trivenitechnologies.in>
2016-04-05 18:45:23 +0530

    working on Jssor slider responsivness

--- app/Scripts/controllers/HomeController.js
+++ app/Scripts/controllers/HomeController.js
@@ -42,11 +42,6 @@
 
 
         // [S] Jssor slider
-        function setJssorSliderSize() {
-            var boxwrap = angular.element("#destination-boxwrap");
-            angular.element('#jssor_slider, #jssor_slider div').css({ width: boxwrap.css('width'), height: boxwrap.css('height') });
-        }
-
         var initJassorSlider = function () {
             var slideshowTransitions = [
               { $Duration: 4000, $Opacity: 2 }
@@ -62,21 +57,52 @@
             };
 
             var JassorSlider = new $JssorSlider$("jssor_slider", jssorOptions);
+            //function ScaleSlider() {
+            //    debugger;
+            //    var refSize = JassorSlider.$Elmt.parentNode.clientWidth;
+            //    if (refSize) {
+            //        //refSize = Math.min(refSize, 600);
+            //        JassorSlider.$ScaleWidth(refSize);
 
-            function ScaleSlider() {
-                var refSize = JassorSlider.$Elmt.parentNode.clientWidth;
-                if (refSize) {
-                    refSize = Math.min(refSize, 600);
-                    JassorSlider.$ScaleWidth(refSize);
-                }
-                else {
-                    window.setTimeout(ScaleSlider, 30);
-                }
+            //        var boxwrap = angular.element("#destination-boxwrap");
+            //        var height = boxwrap.css('height');
+            //        if (w.width() <= 767)
+            //            height = "365px"
+            //        angular.element('#jssor_slider, #jssor_slider div').css({ height: height });
+            //    }
+            //    else {
+            //        window.setTimeout(ScaleSlider, 30);
+            //    }
+            //}
+            //var width;
+            function setJssorSliderSize() {
+                var boxwrap = angular.element("#destination-boxwrap");
+                var height = boxwrap.css('height');
+                if (w.width() <= 767)
+                    height = "365px"
+                //width = parseInt(boxwrap.css('width').replace("px", ''));
+                angular.element('#jssor_slider, #jssor_slider div').css({ width: boxwrap.css('width'), height: height });
+                //JassorSlider.$ScaleWidth(parseInt(boxwrap.css('width').replace("px", ''), parseInt(height.replace("px", ''))));                
+                //_.each(angular.element('#jssor_slider div.item'), function (item) {
+                //    debugger;
+                //    if (angular.element(item).css('transform') != "none") {
+                //        var css = eval(angular.element(item).css('transform'));
+                //        angular.element(item).css({ transform: css })
+                //    }
+                //});
             }
-            $Jssor$.$AddEvent(window, "resize", setJssorSliderSize);
-        };
 
-        setJssorSliderSize();
+            function matrix(a, b, c, d, e, f) {
+                //var arr = [a, b, c, d, e < 0 ? -width : width, f];
+                //return ('matrix(' + arr.join(',') + ')');
+                var arr = [0 + 'px', e < 0 ? -width + 'px' : width + 'px', 0 + 'px'];
+                return 'translate3d(' + arr.join(',') + ')';
+            }
+
+            setJssorSliderSize();
+            $Jssor$.$AddEvent(window, "resize", setJssorSliderSize);
+            $Jssor$.$AddEvent(window, "orientationchange", setJssorSliderSize);
+        };
         initJassorSlider();
         // [E] Jssor slider
     }

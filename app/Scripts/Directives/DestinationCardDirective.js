22f9a936e2e4ae99f04c5d5d0acd3d8deeec6c13
tgsravi <ravi.p@trivenitechnologies.in>
2016-04-29 16:41:10 +0530

    1) save in localStorage only if data available 2) attraction detail popup CSS changed

--- app/Scripts/Directives/DestinationCardDirective.js
+++ app/Scripts/Directives/DestinationCardDirective.js
@@ -53,15 +53,14 @@
                                 $scope.destinationData.returnDate = ConvertToRequiredDate(data.ReturnDateTime, 'UI'),
                                 $scope.destinationData.currencyCode = getCurrencyCode(data.PricedItineraries[0])
                                 $scope.destinationData.currencySymbol = UtilFactory.GetCurrencySymbol($scope.destinationData.currencyCode);
+                                DestinationFactory.setDestinationData(
+                                   {
+                                       Origin: $scope.origin,
+                                       Destination: $scope.destination,
+                                       DepartureDate: $scope.departureDate,
+                                       ReturnDate: $scope.returnDate
+                                   }, data);
                             }
-
-                            DestinationFactory.setDestinationData(
-                                    {
-                                        Origin: $scope.origin,
-                                        Destination: $scope.destination,
-                                        DepartureDate: $scope.departureDate,
-                                        ReturnDate: $scope.returnDate
-                                    }, data);
                             $scope.url = 'f=' + $scope.destinationData.origin + ';t=' + $scope.destinationData.destination + ';d=' + $scope.departureDate + ';r=' + $scope.returnDate;
                         });
                     });

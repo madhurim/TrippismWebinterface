e88414f3bcdc10d61ff3a21a5087d7ad288e05f3
tgsravi <ravi.p@trivenitechnologies.in>
2016-05-16 16:11:18 +0530

    working on hotels on card

---
+++ app/Scripts/controllers/HotelRangeCardController.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+    angular.module('TrippismUIApp').controller('HotelRangeCardController', ['$scope', HotelRangeCardController]);
+    function HotelRangeCardController($scope) {
+        $scope.getMinHotelAvgPrice = getMinHotelAvgPrice;
+        function getMinHotelAvgPrice(hotelList) {
+            if (hotelList.ThreeStar || hotelList.FourStar || hotelList.FiveStar) {
+                var minHotel = _.min(hotelList, function (item) { return item.MinimumPriceAvg || Infinity; });
+                if (minHotel.MinimumPriceAvg != 0.0)
+                    $scope.HotelAvg = minHotel;
+            }
+        }
+    }
+})();

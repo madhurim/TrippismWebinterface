85122f38fd788f6c91807cfc3504646286a1c369
tgsravi <ravi.p@trivenitechnologies.in>
2016-06-15 16:13:32 +0530

    urlConstant changed

--- app/scripts/constants/urlConstant.js
+++ app/scripts/constants/urlConstant.js
@@ -1,20 +1,20 @@
 ﻿(function () {
     'use strict';
     //var hostName = "http://api.trippism.com";
-    var hostName = 'http://dev.trippism.com';
+    //var hostName = 'http://dev.trippism.com';
 
-    //var hostName = 'http://' + window.location.host;
-    //if (angular.lowercase(window.location.host) == "localhost:9000") {
-    //    hostName = 'http://localhost:14606';
-    //}
+    var hostName = 'http://' + window.location.host;
+    if (angular.lowercase(window.location.host) == "localhost:9000") {
+        hostName = 'http://localhost:14606';
+    }
 
     var constants = {
-        //destinationImagePath: 'http://content.trippism.com/images/destinations/',
-        //destinationSmallImagePath: 'http://content.trippism.com/images/destination-small/',
-        //destinationMediumImagePath: 'http://content.trippism.com/images/destination-medium/',
-        destinationImagePath: '../images/destination/',
-        destinationSmallImagePath: '../images/destination-small/',
-        destinationMediumImagePath: '../images/destination-medium/',
+        destinationImagePath: 'http://content.trippism.com/images/destinations/',
+        destinationSmallImagePath: 'http://content.trippism.com/images/destination-small/',
+        destinationMediumImagePath: 'http://content.trippism.com/images/destination-medium/',
+        //destinationImagePath: '../images/destination/',
+        //destinationSmallImagePath: '../images/destination-small/',
+        //destinationMediumImagePath: '../images/destination-medium/',
         viewsPath: '/views/',
         partialViewsPath: '/views/partials/',
         apiURL: hostName + '/api/Sabre/',

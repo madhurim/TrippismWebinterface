b65f9db9170ac18f05ecfa22801c179a036d70a6
tgsravi <ravi.p@trivenitechnologies.in>
2016-06-16 12:38:17 +0530

    scroll issue fixed

--- app/scripts/directives/scrollDirective.js
+++ app/scripts/directives/scrollDirective.js
@@ -13,7 +13,7 @@ angular.module('TrippismUIApp').directive('scroll', function () {
         link: function (scope, element, attrs) {
             var raw = element[0];
             element.bind('scroll', function () {
-                if (raw.scrollTop + raw.offsetHeight == raw.scrollHeight) {
+                if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
                     scope.scrollCallback();
                 }
             });

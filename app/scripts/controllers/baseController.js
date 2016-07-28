895e3600067b0b9ec72920db5f53eeb59f7071a4
tgsravi <ravi.p@trivenitechnologies.in>
2016-07-28 18:47:37 +0530

    working i18n

--- app/scripts/controllers/baseController.js
+++ app/scripts/controllers/baseController.js
@@ -2,16 +2,20 @@
     'use strict';
     var controllerId = 'BaseController';
     angular.module('TrippismUIApp').controller(controllerId,
-        ['$scope', '$modal', '$rootScope', 'UtilFactory', 'urlConstant', BaseController]);
+        ['$scope', '$modal', '$rootScope', '$timeout', 'tmhDynamicLocale', 'UtilFactory', 'urlConstant', 'BaseFactory', BaseController]);
 
-    function BaseController($scope, $modal, $rootScope, UtilFactory, urlConstant) {
+    function BaseController($scope, $modal, $rootScope, $timeout, tmhDynamicLocale, UtilFactory, urlConstant, BaseFactory) {
         $rootScope.isShowAlerityMessage = true;
-
         init();
+        //getLocale();
         function init() {
             UtilFactory.ReadAirportJson();
             UtilFactory.GetCurrencySymbols();
             UtilFactory.ReadHighRankedAirportsJson();
+        }
+        function getLocale() {
+            BaseFactory.getLocale().then(function (data) {
+            });
         }
 
         $scope.aboutUs = function () {
@@ -32,7 +36,11 @@
         $scope.$on('bodyClass', function (event, args) {
             $scope.bodyClass = args;
         });
+
+        $('#select-i18n').ddslick({
+            onSelected: function (data) {
+                $timeout(function () { tmhDynamicLocale.set(data.selectedData.value); }, 0, true);
+            }
+        });
     }
 })();
-
-
---
+++ app/scripts/factories/baseFactory.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    'use strict';
+    angular.module('TrippismUIApp').factory('BaseFactory', ['$http', '$q', 'LocalStorageFactory', 'dataConstant', BaseFactory]);
+    function BaseFactory($http, $q, LocalStorageFactory, dataConstant) {
+        var localPromise;
+        return {
+            getLocale: getLocale
+        }
+        function getLocale() {
+            //"{"ip":"43.243.38.6","hostname":"No Hostname","city":"Sarkhej","region":"Gujarat","country":"IN","loc":"22.9833,72.5000"
+            //,"org":"AS133226 VISION SMARTLINK NETWORKING PRIVATE LIMITED"}"
+            var localData = LocalStorageFactory.get(dataConstant.userLocaleLocalStorage);
+            if (localData) {
+                return localData;
+            }
+
+            if (localPromise) {
+                $q.when(localPromise).then(function (data) { return data });
+            }
+            return localPromise = $http.get('http://ipinfo.io').then(function (data) {
+                if (data.status == 200) {
+                    data = {
+                        ip: data.ip,
+                        hostName: data.hostname,
+                        city: data.city,
+                        region: data.region,
+                        country: data.country,
+                        location: {
+                            lat: data.loc ? data.loc.split(',')[0] : null,
+                            lng: data.loc ? data.loc.split(',')[1] : null
+                        }
+                    };
+                    LocalStorageFactory.save(dataConstant.userLocaleLocalStorage, data);
+                    return data;
+                }
+                else
+                    return null;
+            }, function () { return null; });
+        }
+    }
+})();
